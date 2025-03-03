cabfdc0af83d49c7c298351c00c119a43a573004
MPrasad1611 <143625599+MPrasad1611@users.noreply.github.com>
2025-03-03 22:19:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
 
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.method === "POST") {
+    let ipData = "";
+
+
+    req.on("data", (chunk) => {
+      ipData += chunk.toString();
+    });
+
+    req.on("end", () => {
+      
+      fs.readFile("./hello.json", "utf8", (err, data) => {
+        if (err) {
+          res.end("Error reading file");
+          return;
+        }
+
+     
+        let existdata = JSON.parse(data);
+
+        
+        const newData = JSON.parse(ipData);
+
+        
+        const emailExists = existdata.some((item) => item.email === newData.email);
+
+        if (emailExists) {
+         
+          res.end("Email already exists");
+        } else {
+          
+          existdata.push(newData);
+          let updatedata = JSON.stringify(existdata, null, 2);
+
+
+          fs.writeFile("./hello.json", updatedata, (err) => {
+            if (err) {
+              res.end("Error writing file");
+            } else {
+              res.end("Data updated successfully");
+            }
+          });
+        }
+      });
+    });
+  } else {
+    
+    res.end("Method Not Allowed");
+  }
+});
+
+server.listen(5646, () => {
+  console.log("Server is running ");
+});
